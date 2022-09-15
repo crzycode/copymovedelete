d96d3a3cbd1e3b69db19d761d3a3a268abfb3893
psydeveloper <cb.mangalsingh@gmail.com>
2022-09-16 03:06:37 +0530

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,10 @@
+﻿string source = @"D:\Intervies\";
+string distination = @"D:\Interview Que\";
+if (File.Exists(distination + "INTERVIEW.docx"))
+{
+    Console.WriteLine("file is already exist");
+}
+else
+{
+    File.Copy(source + "INTERVIEW.docx", distination + "INTERVIEW.docx");
+}
